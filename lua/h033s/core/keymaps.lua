7fc42a17641c7a25dfd579dfc0071b12030a2f8f
Antonio Nazco <tonito.nazco@gmail.com>
2024-07-12 22:58:43 -0400

    Terminal and Scrolling Keymaps

--- lua/h033s/core/keymaps.lua
+++ lua/h033s/core/keymaps.lua
@@ -5,7 +5,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 -- Windows Management
 vim.keymap.set('n', 'wr', '<C-w>v', { desc = 'Split to the Right' })
 vim.keymap.set('n', 'wb', '<C-w>s', { desc = 'Split to the Bottom' })
@@ -20,17 +19,29 @@ vim.keymap.set('n', '<leader>wr', '<cmd>tabnew<CR>', { desc = 'Open a new tab' }
 vim.keymap.set('n', '<leader>wq', '<cmd>tabclose<CR>', { desc = 'Close current tab' })
 vim.keymap.set('n', '<leader>wl', '<cmd>tabn<CR>', { desc = 'Go to next tab' })
 vim.keymap.set('n', '<leader>wh', '<cmd>tabp<CR>', { desc = 'Go to previous tab' })
-
+-- Line Management
 vim.keymap.set('n', '<S-CR>', 'A<CR><ESC>', { desc = 'Create a new Line' })
 vim.keymap.set('n', '<C-CR>', 'i<CR><ESC>', { desc = 'Create a new Line since cursor position' })
-
-
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move Line one line to bottom' })
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv", { desc = 'Move Line one line to top' })
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set('n', 'n', 'nzzzv', {desc = "Next Ocurrence in Middle Screen"})
+vim.keymap.set('n', 'n', 'nzzzv', {desc = "Next Ocurrence in Middle Screen"})
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {desc = "Scroll Down Keeping Cursor in the Middle"})
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {desc = "Scroll Up Keeping Cursor in the Middle"})
+
+-- Yanks
+vim.keymap.set({'v', 'n'}, 'Y', 'y$')
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set({"n", "v"}, "<leader>Y", [["+Y]])
+-- Teminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {desc = "Close Terminal"})
+vim.keymap.set("n", "<leader>t", "<cmd>bel term<CR>")
+vim.keymap.set("n", "<leader>tw", function()
+    local bufferFilePath = vim.fn.expand("%")
+    local bufferDirectory = vim.fn.fnamemodify(bufferFilePath, ":p:h")
+    vim.cmd(string.format(":botright new | call termopen(&shell, #{cwd: '%s'})", bufferDirectory))
+end)
 
 vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
