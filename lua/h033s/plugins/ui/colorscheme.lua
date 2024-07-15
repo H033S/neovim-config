e15ade42f4a1e789d8b1760c5ff2519273ab6bb8
Antonio Nazco <tonito.nazco@gmail.com>
2024-07-14 21:58:16 -0400

    Resolving colorscheme

--- lua/h033s/plugins/ui/colorscheme.lua
+++ lua/h033s/plugins/ui/colorscheme.lua
@@ -7,13 +7,25 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+<<<<<<< HEAD
         --vim.g.sonokai_enable_italic = true
         vim.cmd.colorscheme('nordic')
         vim.inspect(vim.g.lightline)
+=======
+>>>>>>> e44c4e6 (Changed colorscheme to Nordic)
 
-        vim.cmd [[g:lightline = { 'colorscheme': 'tender' }]]
-        vim.api.nvim_set_hl(0, 'Normal', {bg = 'none'})
-        vim.api.nvim_set_hl(0, 'NormalFloat', {bg = 'none'})
+        local colorForSelection = "#F6C987"
+        require('nordic').setup({
+            transparent_bg = true,
+            cursorline = {
+                theme = 'light'
+            }
+        })
+
+        vim.cmd.colorscheme('nordic')
+        vim.cmd [[g:lightline = { 'colorscheme': 'nordic' }]]
+        -- vim.api.nvim_set_hl(0, 'Normal', {bg = 'none'})
+        -- vim.api.nvim_set_hl(0, 'NormalFloat', {bg = 'none'})
     end
 }
 
